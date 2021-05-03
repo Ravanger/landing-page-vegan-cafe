d2ae1b397791efae957462637f660a994861ef9b
Ravanger <Boris.Rozzy@gmail.com>
2021-05-03 01:41:56 -0400

    Landing Page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,23 @@
 import ContentBox from "../common/ContentBox"
+import Spacer from "../common/Spacer"
 
 const LandingPage = () => {
   return (
     <ContentBox id="landing">
-      <h1>Landing</h1>
+      <div className="flex flex-col items-center">
+        <h1 className="text-6xl">The Green Goose</h1>
+        <span className="self-end text-2xl">Vegan café</span>
+        <Spacer size="2rem" />
+        <a
+          href="https://placeholder.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-green py-4 px-16"
+        >
+          Menu
+          <span className="sr-only">Opens in new window</span>
+        </a>
+      </div>
     </ContentBox>
   )
 }
--- src/components/common/ContentBox/ContentBox.tsx
+++ src/components/common/ContentBox/ContentBox.tsx
@@ -1,7 +1,7 @@
 import { ContentBoxPropsType } from "./ContentBox.types"
 
 const ContentBox = (props: ContentBoxPropsType) => (
-  <div className="h-screen flex flex-col justify-center" id={props.id}>
+  <div className="h-screen flex flex-col justify-center w-full" id={props.id}>
     <section className="w-full flex flex-col items-center bg-darkGray bg-opacity-60 py-8 px-4 text-white">
       {props.children}
     </section>
