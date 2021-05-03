fc04ad48c4209347f65bb68802807ce6fe264027
Ravanger <Boris.Rozzy@gmail.com>
2021-05-02 22:34:23 -0400

    Components folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import LandingPage from "../LandingPage"
-import AboutUsPage from "../AboutUsPage"
-import HoursPage from "../HoursPage"
-import ContactPage from "../ContactPage"
+import LandingPage from "../components/LandingPage"
+import AboutUsPage from "../components/AboutUsPage"
+import HoursPage from "../components/HoursPage"
+import ContactPage from "../components/ContactPage"
 
 const Home = () => {
   return (
