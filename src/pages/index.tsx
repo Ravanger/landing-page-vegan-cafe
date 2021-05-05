e92caf58675b0d84ea1fb5ea0638bb53a3072681
Ravanger <Boris.Rozzy@gmail.com>
2021-05-05 15:26:28 -0400

    Add Spacers between sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,18 @@ import AboutUsPage from "../components/AboutUsPage"
 import HoursPage from "../components/HoursPage"
 import ContactPage from "../components/ContactPage"
 import BackgroundImage from "../components/common/BackgroundImage"
+import Spacer from "../components/common/Spacer"
 
 const Home = () => {
   return (
     <div className="flex flex-col">
       <BackgroundImage />
       <LandingPage />
+      <Spacer />
       <AboutUsPage />
+      <Spacer />
       <HoursPage />
+      <Spacer />
       <ContactPage />
     </div>
   )
