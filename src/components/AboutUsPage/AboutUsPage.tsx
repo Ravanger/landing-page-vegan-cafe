c750405c0c44756b821f08eb7c4aa914cfd47cf3
Ravanger <Boris.Rozzy@gmail.com>
2021-05-10 21:45:57 -0400

    About us content

--- src/components/AboutUsPage/AboutUsPage.tsx
+++ src/components/AboutUsPage/AboutUsPage.tsx
@@ -7,18 +7,21 @@ const AboutUsPage = () => {
       <div className="text-lg sm:text-2xl">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suscipit
-          tellus sed quam purus placerat risus donec. Mauris eu sed posuere
-          fermentum, convallis est, mi. Ipsum semper et sed dignissim tortor.
+          tellus sed quam purus placerat risus donec.
+        </p>
+        <Spacer size="1rem" />
+        <p>
+          Mauris eu sed posuere fermentum, convallis est, mi. Ipsum semper et
+          sed dignissim tortor.
+        </p>
+        <Spacer size="1rem" />
+        <p>
           Morbi ornare nec quis pretium quisque mattis gravida cursus. Molestie
           sed nisi mauris erat purus arcu ac elementum.
         </p>
-        <Spacer size="2rem" />
-        <p>
-          Auctor sed ultrices lacus augue morbi dui at. Maecenas gravida semper
-          cursus tellus lectus integer lacus. Mattis odio sagittis magna libero
-          ipsum lorem at nulla. Faucibus consectetur viverra id arcu, diam.
-          Commodo mauris semper cursus feugiat diam, lectus vitae tellus
-          elementum.
+        <Spacer size="4rem" />
+        <p className="font-bold text-center">
+          Auctor sed ultrices lacus augue morbi dui at.
         </p>
       </div>
     </ContentBox>
