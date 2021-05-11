848cfd9ebb543f509d84743faf55f6689eed5b97
Ravanger <Boris.Rozzy@gmail.com>
2021-05-10 20:58:10 -0400

    Footer responsiveness

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import Socials from "../Socials"
 
 const Footer = () => (
-  <footer className="hidden md:flex flex-row fixed bottom-0 left-0 right-0 bg-darkGray bg-opacity-95 items-center justify-between text-white px-4">
+  <footer className="hidden sm:flex flex-row fixed bottom-0 left-0 right-0 bg-darkGray bg-opacity-95 items-center justify-between text-white px-4">
     <span className="text-xl lg:text-2xl">
       <a href="tel:+15551234567">(555) - 123 - 4567</a>
     </span>
